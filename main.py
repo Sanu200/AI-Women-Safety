ddfdc8c02cae831f919b11afa5b613b97152c5a6
Sanu200 <158201133+Sanu200@users.noreply.github.com>
2024-09-16 22:32:11 +0530

    Update main.py
    Optimizing the Video capture frame rate

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-# main.py
 import cv2
-from src.detection import load_yolo_model, load_gender_model,detect_people_and_classify_gender
+import time
+from src.detection import load_yolo_model, load_gender_model, detect_people_and_classify_gender
 from src.alarm import trigger_alarm  # Trigger the alarm sound
 from src.save_video import save_video_clip  # Save video clips
 from src.email_notification import send_email  # Send email notifications
-import time
+
+# Set the dimensions for the video feed
+wCam, hCam = 640, 480
 
 def main():
-    video_capture = cv2.VideoCapture(0)  # Open default camera
-
+    # Open default camera and set width/height
+    video_capture = cv2.VideoCapture(0)
+    video_capture.set(3, wCam)
+    video_capture.set(4, hCam)
+    
     yolo_net = load_yolo_model()
     gender_model = load_gender_model()
 
     # Variables to control recording state and alarm triggers
     alarm_triggered = False
     recording_start_time = None
+    pTime = 0
 
     while True:
         ret, frame = video_capture.read()
@@ -24,17 +30,16 @@ def main():
         # Detect people and classify gender in the frame
         people, genders = detect_people_and_classify_gender(frame, yolo_net, gender_model)
 
-          # Draw bounding boxes and gender labels on the frame
+        # Draw bounding boxes and gender labels on the frame
         for idx, (x, y, w, h) in enumerate(people):
-            label = genders[idx] if idx < len(genders) else 'unkown'  # Gender of the detected person
+            label = genders[idx] if idx < len(genders) else 'unknown'  # Gender of the detected person
             color = (255, 0, 0) if label == 'Man' else (0, 255, 0)  # Blue for male, green for female
             
-            #Draw rectangle and put label on the frame
+            # Draw rectangle and put label on the frame
             cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
             cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
 
-
-        # Check if an alarm needs to be triggered
+        # Trigger the alarm and save the video if conditions are met
         if len(people) > 1 and 'Man' in genders and 'Woman' in genders:
             if not alarm_triggered:
                 alarm_triggered = True
@@ -48,7 +53,13 @@ def main():
 
                 # Send email to authorities with video clip
                 send_email("EMAIL_RECEIVER", "Alarm Triggered", "Video clip attached.", "saved_clips/")
-        
+
+        # Calculate and display FPS
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(frame, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+
         # Display the real-time feed with bounding boxes and labels
         cv2.imshow('Video Feed', frame)
 
