220707bef31b84d019e5570df2ca3ecb3ee3e4d9
Sanu200 <158201133+Sanu200@users.noreply.github.com>
2024-09-17 11:15:55 +0530

    Update alarm.py

--- src/alarm.py
+++ src/alarm.py
@@ -1,5 +1,5 @@
-# src/alarm.py
 from playsound import playsound
 
 def trigger_alarm():
-    playsound('C:/Users/sanun/Downloads/ThePRO_JECT 2/alarm/alarm.mp3')  # Path to the alarm sound file
+    # Play the alarm sound
+    playsound("C:/Users/sanun/Downloads/ThePRO_JECT 2/alarm/alarm.wav")
